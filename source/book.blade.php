f9055248e46bfe4827d4819ed74573c8c6b48a32
Michael Joyner <joyner.michael@gmail.com>
2021-12-24 11:18:17 +0800

    fix image width

--- source/book.blade.php
+++ source/book.blade.php
@@ -18,7 +18,7 @@
     <p class="h2 text-center">Grant Fowlds and Graham Spence</p>
     <p class="mt-12 max-w-md mx-auto">Conservationist Grant Fowlds lives to save and protect Africa’s rhinos, elephants and other iconic wildlife, to preserve their habitats, to increase their range and bring back the animals where they have been decimated by decades of war, as in Angola, Mozambique and the Democratic Republic of the Congo.</p>
     <p class="mt-6 mb-12 max-w-md mx-auto">This vivid account of his work tells of a fellow conservationist tragically killed by the elephants he was seeking to save and a face-off with poachers, impoverished rural people exploited by rapacious local businessmen.</p>
-    <img src="/assets/images/home/book_rewilding.jpg" alt="Book cover for Rewilding Africa by Grant Fowlds and Graham Spence" class="block my-20 mx-auto max-w-lg">
+    <img src="/assets/images/home/book_rewilding.jpg" alt="Book cover for Rewilding Africa by Grant Fowlds and Graham Spence" class="block my-20 mx-auto w-full max-w-lg">
 
     <p class="my-12 max-w-md mx-auto">This the second book by Grant and Graham Spence, who have once again made a great team in lucidly revealing the endeavours of Grant.</p>
     <p class="h1 mt-20 mb-12 text-center">Available Soon</p>
