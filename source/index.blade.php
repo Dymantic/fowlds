384589ad783e69f7572112da3f95a1763e494594
Michael Joyner <joyner.michael@gmail.com>
2024-12-11 11:24:59 +0800

    hide ig section

--- source/index.blade.php
+++ source/index.blade.php
@@ -12,7 +12,6 @@
 @include('_layouts.home.quote')
 @include('_layouts.home.introduction')
 @include('_layouts.home.book')
-@include('_layouts.home.instagram')
 @include('_layouts.home.donate')
 @include('_layouts.home.blog')
 @endsection
